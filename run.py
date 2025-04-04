3971363d5dcc11693977f505e94f2c8c37fbcbca
HADEON <52324046+h8d13@users.noreply.github.com>
2025-04-04 14:03:21 +0200

    Update run.py

--- run.py
+++ run.py
@@ -8,7 +8,6 @@ import sys
 image = "alpine:latest" 
 short_project_uuid = "266749"
 pprefix = "sudo"
-proj_dir = "Projects"
 host_port = "443"
 container_port = "443"
 
@@ -22,7 +21,7 @@ def start_docker():
     subprocess.run(command, shell=True)
 
 def build_custom_image():
-    dockerfile_dir = f"./{proj_dir}/{short_project_uuid}"
+    dockerfile_dir = f"./{short_project_uuid}"
     custom_image_name = f"custom-{short_project_uuid}"
     print(f"Building custom Docker image from Dockerfile...")
     command = f"{pprefix} service docker start && {pprefix} docker build -t {custom_image_name} {dockerfile_dir}"
@@ -38,7 +37,7 @@ def create_new_container(custom_image):
     print(f"Creating new container from custom image...")
     command = f"{pprefix} docker rm {short_project_uuid} 2>/dev/null || true"
     subprocess.run(command, shell=True)
-    command = f"{pprefix} docker run -p {host_port}:{container_port} -v ./{proj_dir}/{short_project_uuid}:/app{short_project_uuid} -it --name {short_project_uuid} {custom_image}"
+    command = f"{pprefix} docker run -p {host_port}:{container_port} -v ./{short_project_uuid}:/app{short_project_uuid} -it --name {short_project_uuid} {custom_image}"
     subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
